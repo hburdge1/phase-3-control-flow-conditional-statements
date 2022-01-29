f93beca0497b77395c0761a45bb1116e9ad75edc
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-01-28 20:49:12 -0800

    commit tree f85ea8b7adafcd2e617c3bc4e5c3f196a9f5eacf

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,44 @@
 def admin_login(username, password)
-  # your code here
+    username=username.downcase
+  if (username == "admin" ) && (password == "12345")
+    return "Access granted"
+  else
+   return "Access denied"  
+  end
+
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40 then
+    return "It's brisk out there!"
+  elsif 40 < temperature && temperature < 65 then
+    return "It's a little chilly out there!"
+  elsif 85 < temperature then
+    return "It's too dang hot out there!"
+  else
+    return "It's perfect out there!"
+end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 != 0 then
+    return "Fizz"
+  elsif num % 5 == 0 && num % 3 != 0 then
+    return "Buzz"
+  elsif num % 3 == 0 && num % 5 == 0 then
+    return "FizzBuzz"
+  else
+    return num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+" then return num1+num2
+  when "-" then return num1-num2
+  when "/" then return num1/num2
+  when "*" then return num1*num2
+  else puts "Invalid operation!"
+  end
 end
 
